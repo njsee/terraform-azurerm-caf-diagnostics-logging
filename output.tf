c86cf14a960ac8d7519c9eb5edb70c9e7c80ef3c
njsee <48836977+njsee@users.noreply.github.com>
2020-11-06 18:09:57 -0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -7,13 +7,11 @@ output "diagnostics_map" {
         azurerm_storage_account.log, 
         ]
 
-    value = "${
-        map(
+    value = map(
             "diags_sa", azurerm_storage_account.log.id,
             "diags_sa_name", azurerm_storage_account.log.name,
             "diags_sa_rg", var.resource_group_name,
             "eh_name",  var.enable_event_hub == true ? azurerm_eventhub_namespace.log[0].name : null,
             "eh_id", var.enable_event_hub == true ? azurerm_eventhub_namespace.log[0].id : null,
         )
-    }"
 }
